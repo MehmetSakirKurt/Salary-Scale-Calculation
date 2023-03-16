d8e97a8d7e9eb2fbcc1c6593f12b1239b23a6630
MehmetSakirKurt <mehmet_wolf571@hotmail.com>
2023-03-17 00:05:41 +0300

    LinearRegression

---
+++ Proje3.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+df = pd.read_csv("polynomial.csv",sep = ";")
+df
+
+polynomial_regression = PolynomialFeatures(degree = 5)
+
+x_polynomial = polynomial_regression.fit_transform(df[['deneyim']])
+
+reg = LinearRegression()
+reg.fit(x_polynomial,df['maas'])
+y_head = reg.predict(x_polynomial)
+plt.plot(df['deneyim'],y_head,color= "red",label = "polynomial regression")
+
+plt.legend()
+
+plt.scatter(df['deneyim'],df['maas'])   
+
+plt.show()
+
+x_polynomial1 = polynomial_regression.fit_transform([[4.5]])
+reg.predict(x_polynomial1)
